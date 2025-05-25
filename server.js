40028dee07a7fe31d5e816d8d0b18b79716dad63
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-05-25 08:51:31 +0700

    first commit2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['https://swe-project-backend-g841.onrender.com'],
+  origin: ['https://swe-project-frontend.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
